a2e08fdad8b9f49b170d179f74ca725103c88553
Maïssa Nouhaud <maissa2.nouhaud@epitech.eu>
2021-04-08 10:24:11 +0200

    [*]3_inventory_migration.js : doing the test

--- migrations/3_inventory_migration.js
+++ migrations/3_inventory_migration.js
@@ -5,13 +5,7 @@ module.exports = async (deployer, _, accounts) => {
     deployer.deploy(
         Inventory,
         {
-            originator: accounts[0],
-            owner: accounts[0],
             inventory: MichelsonMap.fromLiteral({})
-        },
-        accounts[0],
-        {
-            overwrite: true
         }
     );
 };
