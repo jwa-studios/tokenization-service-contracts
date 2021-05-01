fa503477c06411b292034965b025d44640e06332
Olivier Scherrer <pode.fr@gmail.com>
2021-05-01 16:24:33 -0400

    Prepare 0.0.7 release
    - Also publish inventory contract

--- scripts/prepareDist.js
+++ scripts/prepareDist.js
@@ -7,6 +7,10 @@ const warehouseContractJSON = JSON.parse(
     String(fs.readFileSync(`${contractsFolder}/Warehouse.json`))
 );
 
+const inventoryContractJSON = JSON.parse(
+    String(fs.readFileSync(`${contractsFolder}/Inventory.json`))
+);
+
 if (!fs.existsSync(distFolder)) {
     fs.mkdirSync(distFolder);
 }
@@ -14,6 +18,11 @@ if (!fs.existsSync(distFolder)) {
 fs.writeFileSync(
     `${distFolder}/warehouse.json`,
     stringifiedContract(warehouseContractJSON)
+);
+
+fs.writeFileSync(
+    `${distFolder}/inventory.json`,
+    stringifiedContract(inventoryContractJSON)
 );
 
 function stringifiedContract(contractJSON) {
